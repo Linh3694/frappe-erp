971f2fac718dd56659ef933bf58180178ff5beea
Linh3694 <88594329+Linh3694@users.noreply.github.com>
2025-08-23 09:46:23 +0700

    update

---
+++ erp/crm/doctype/crm_family_relationship/crm_family_relationship.py
@@ -0,0 +1,27 @@
+# Copyright (c) 2024, Frappe Technologies Pvt. Ltd. and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe.model.document import Document
+
+
+class CRMFamilyRelationship(Document):
+	# begin: auto-generated types
+	# This code is auto-generated. Do not modify anything in this block.
+
+	from typing import TYPE_CHECKING
+
+	if TYPE_CHECKING:
+		from frappe.types import DF
+
+		access: DF.Check
+		guardian: DF.Link | None
+		key_person: DF.Check
+		parent: DF.Data
+		parentfield: DF.Data
+		parenttype: DF.Data
+		relationship_type: DF.Literal["dad", "mom", "foster_parent", "grandparent", "uncle_aunt", "sibling", "other"]
+		student: DF.Link | None
+	# end: auto-generated types
+
+	pass
