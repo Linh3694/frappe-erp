2420389877ee772e2ddae499e204ad76528249b3
Linh3694 <hailinh.n23@gmail.com>
2025-08-25 09:06:54 +0700

    update

--- erp/api/erp_sis/__init__.py
+++ erp/api/erp_sis/__init__.py
@@ -1,2 +1,6 @@
 # Copyright (c) 2024, Wellspring International School and contributors
 # For license information, please see license.txt
+
+from .family import *
+from .student import *
+from .guardian import *
