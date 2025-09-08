4107afaefb8c5e513bba9e99cbd5dccc6e4450b8
Linh3694 <hailinh.n23@gmail.com>
2025-09-08 17:07:14 +0700

    update

---
+++ erp/sis/doctype/sis_report_card_evaluation_scale/sis_report_card_evaluation_scale.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Wellspring and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe.model.document import Document
+
+
+class SISReportCardEvaluationScale(Document):
+    pass
