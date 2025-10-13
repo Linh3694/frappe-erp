d920291e9276100304908b28968131971396460b
Linh3694 <hailinh.n23@gmail.com>
2025-10-13 09:08:52 +0700

    update

--- erp/sis/doctype/sis_bus_transportation/sis_bus_transportation.py
+++ erp/sis/doctype/sis_bus_transportation/sis_bus_transportation.py
@@ -8,7 +8,6 @@ from frappe.model.document import Document
 class SISBusTransportation(Document):
 	def validate(self):
 		self.validate_unique_fields()
-		self.validate_driver_exists()
 
 	def validate_unique_fields(self):
 		"""Validate unique fields"""
@@ -25,9 +24,3 @@ class SISBusTransportation(Document):
 				"name": ("!=", self.name)
 			}):
 				frappe.throw("Biển số đã tồn tại")
-
-	def validate_driver_exists(self):
-		"""Validate that the driver exists"""
-		if self.driver_id:
-			if not frappe.db.exists("SIS Bus Driver", self.driver_id):
-				frappe.throw("Tài xế không tồn tại")
