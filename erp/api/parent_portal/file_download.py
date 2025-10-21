de752a5c98b8e5b6dade3cf64ff2c385e59af888
Linh3694 <hailinh.n23@gmail.com>
2025-10-21 13:54:48 +0700

    update

--- erp/api/parent_portal/file_download.py
+++ erp/api/parent_portal/file_download.py
@@ -31,7 +31,7 @@ def _get_current_parent():
 	return guardian
 
 
-@frappe.whitelist()
+@frappe.whitelist(allow_guest=True)
 def download_leave_attachment():
 	"""
 	Download a leave request attachment file - PARENT PORTAL ONLY
