058e749036e10e985931a607f9d193981b9f2841
Linh3694 <hailinh.n23@gmail.com>
2025-12-12 16:58:46 +0700

    update

---
+++ erp/sis/doctype/sis_library_event/sis_library_event.py
@@ -0,0 +1,10 @@
+# Copyright (c) 2025, Wellspring and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe.model.document import Document
+
+
+class SISLibraryEvent(Document):
+	"""Sự kiện thư viện - quản lý hoạt động, triển lãm, sự kiện đặc biệt."""
+	pass
---
+++ erp/sis/doctype/sis_library_event_day/sis_library_event_day.py
@@ -0,0 +1,10 @@
+# Copyright (c) 2025, Wellspring and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe.model.document import Document
+
+
+class SISLibraryEventDay(Document):
+	"""Ngày của sự kiện thư viện - child table."""
+	pass
