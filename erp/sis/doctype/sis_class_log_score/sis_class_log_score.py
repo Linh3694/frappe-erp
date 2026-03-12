523913d2053a0df89074a05ef015175a09fa130a
Linh3694 <hailinh.n23@gmail.com>
2026-03-12 09:32:40 +0700

    update

--- erp/sis/doctype/sis_class_log_score/sis_class_log_score.py
+++ erp/sis/doctype/sis_class_log_score/sis_class_log_score.py
@@ -1,8 +1,43 @@
 import frappe
 from frappe.model.document import Document
+from frappe import _
 
 
 class SISClassLogScore(Document):
-    pass
+    def validate(self):
+        """Đảm bảo title_vn và title_en là duy nhất trong cùng type + education_stage"""
+        if not self.title_vn or not self.title_vn.strip():
+            return
+
+        title_vn = self.title_vn.strip()
+        title_en = (self.title_en or "").strip()
+
+        def _filters():
+            f = [
+                ["type", "=", self.type],
+                ["is_active", "=", 1],
+            ]
+            if self.name:
+                f.append(["name", "!=", self.name])
+            if self.education_stage:
+                f.append(["education_stage", "=", self.education_stage])
+            else:
+                f.append(["or", [["education_stage", "=", ""], ["education_stage", "is", "not set"]]])
+            return f
+
+        # Kiểm tra trùng title_vn
+        f_vn = _filters() + [["title_vn", "=", title_vn]]
+        if frappe.get_all("SIS Class Log Score", filters=f_vn, limit=1):
+            frappe.throw(
+                _("Tên (VI) \"{0}\" đã tồn tại trong cùng loại và cấp học").format(title_vn)
+            )
+
+        # Kiểm tra trùng title_en (chỉ khi có giá trị)
+        if title_en:
+            f_en = _filters() + [["title_en", "=", title_en]]
+            if frappe.get_all("SIS Class Log Score", filters=f_en, limit=1):
+                frappe.throw(
+                    _("Tên (EN) \"{0}\" đã tồn tại trong cùng loại và cấp học").format(title_en)
+                )
 
 
