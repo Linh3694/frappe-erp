86a01e74d701334ff40d8b4e9399c494bca8308d
Linh3694 <hailinh.n23@gmail.com>
2026-03-19 08:45:21 +0700

    update

---
+++ erp/sis/doctype/sis_discipline_violation_class_point/sis_discipline_violation_class_point.py
@@ -0,0 +1,12 @@
+# Copyright (c) 2025, Wellspring International School and contributors
+# For license information, please see license.txt
+
+"""
+Child table: Quy tắc điểm trừ cho lớp (Số lần vi phạm | Cấp độ | Điểm)
+"""
+
+from frappe.model.document import Document
+
+
+class SISDisciplineViolationClassPoint(Document):
+    pass
---
+++ erp/sis/doctype/sis_discipline_violation_student_point/sis_discipline_violation_student_point.py
@@ -0,0 +1,12 @@
+# Copyright (c) 2025, Wellspring International School and contributors
+# For license information, please see license.txt
+
+"""
+Child table: Quy tắc điểm trừ cho học sinh (Số lần vi phạm | Cấp độ | Điểm)
+"""
+
+from frappe.model.document import Document
+
+
+class SISDisciplineViolationStudentPoint(Document):
+    pass
